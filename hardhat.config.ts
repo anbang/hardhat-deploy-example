fd1b214f10edd22be10d6a02c2c045a8cae4c220
Anbang <79228807@qq.com>
2023-06-09 18:14:29 +0800

    demo

---
+++ hardhat.config.ts
@@ -0,0 +1,31 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
+
+const config: HardhatUserConfig = {
+	networks: {
+		hardhat: {
+			allowUnlimitedContractSize: true,
+		},
+		okchain: {
+			url: process.env.RPC_URL_OK,
+			accounts: [process.env.PK_ACCOUNT_1],
+			timeout: 600000,
+			blockGasLimit: 0x1fffffffffffff,
+			throwOnTransactionFailures: true,
+			throwOnCallFailures: true,
+			allowUnlimitedContractSize: true,
+		},
+	},
+	solidity: {
+		version: "0.8.18",
+		settings: {
+			optimizer: {
+				enabled: true,
+				runs: 200,
+			},
+		},
+	},
+};
+
+export default config;
